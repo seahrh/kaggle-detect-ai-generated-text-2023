ea4a2f713d936ca77c0d2b4383f29ff39c72b5cf
Ruhong <seahrh@gmail.com>
2024-01-21 16:07:18 +0800

    use drcat external dataset, remove lemma in preprocess

--- src/daigt/preprocess/en/__init__.py
+++ src/daigt/preprocess/en/__init__.py
@@ -1,9 +1,8 @@
-from typing import AnyStr, Set
+from typing import AnyStr
 
 import numpy as np
 import scml
 import scml.nlp as snlp
-import spacy
 
 from daigt.preprocess import Preprocessor
 
@@ -107,10 +106,6 @@ class BasicPreprocessor(Preprocessor):
 class BowPreprocessor(Preprocessor):
     def __init__(self):
         super().__init__()
-        self.nlp = spacy.load("en_core_web_sm", exclude=["textcat", "ner", "tok2vec"])
-        log.debug(self.nlp.pipe_names)
-        self.stops: Set[str] = self.nlp.Defaults.stop_words
-        log.debug(f"{len(self.stops)} stopwords\n{sorted(list(self.stops))}")
         self.contraction = snlp.ContractionExpansion()
 
     def __call__(self, s: AnyStr, drop_stopword: bool = False, **kwargs) -> str:
@@ -118,14 +113,6 @@ class BowPreprocessor(Preprocessor):
         res = self.contraction.apply(res)
         # Expand contractions before removing punctuation
         res = snlp.strip_punctuation(res, replacement=" ")
-        doc = self.nlp(res)
-        tokens = []
-        for token in doc:
-            # some lemma has uppercase char
-            t = token.lemma_.lower()
-            if drop_stopword and t in self.stops:
-                continue
-            tokens.append(t)
-        res = " ".join(tokens)
+        res = res.lower()
         res = snlp.collapse_whitespace(res)
         return res
